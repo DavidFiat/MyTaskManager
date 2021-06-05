f48e46ed578822bdd2be36297d02fad0ba346deb
David <davidfiat24@gmail.com>
2021-06-05 05:05:39 -0500

    Implementing front

---
+++ target/MyTaskManager/js/Task.js
@@ -0,0 +1,11 @@
+class Task{
+
+    constructor(status,name, description, date){
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.description = description;
+        this.date = date;
+    }
+
+}
